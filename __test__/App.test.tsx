575e56f939a3894d4b834f6acb4eaba5a54ce932
Antoine Chalifour <antoine.chalifour@octo.com>
2021-10-19 10:09:22 +0200

    test: améliore le coverage (toggling de todos)

--- __test__/App.test.tsx
+++ __test__/App.test.tsx
@@ -7,10 +7,22 @@ import { App } from "../src/App";
 import userEvent from "@testing-library/user-event";
 
 describe("<App /> tests de caractérisation", function () {
-  it("renders without crashing", () => {
+  it("permet d'ajouter et de cocher des todos", () => {
     render(<App />);
 
     userEvent.type(screen.getByLabelText("Add a new todo"), "Buy bread");
     userEvent.click(screen.getByRole("button", { name: "Add" }));
+    expect(
+      screen.getByRole("checkbox", { name: "Buy bread" })
+    ).not.toBeChecked();
+
+    userEvent.type(screen.getByLabelText("Add a new todo"), "Buy milk");
+    userEvent.click(screen.getByRole("button", { name: "Add" }));
+    expect(
+      screen.getByRole("checkbox", { name: "Buy milk" })
+    ).not.toBeChecked();
+
+    userEvent.click(screen.getByRole("checkbox", { name: "Buy milk" }));
+    expect(screen.getByRole("checkbox", { name: "Buy milk" })).toBeChecked();
   });
 });
