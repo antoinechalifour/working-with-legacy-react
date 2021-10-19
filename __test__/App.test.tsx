10e55b12763acdc71fdb3480180d26f56247ba05
Antoine Chalifour <antoine.chalifour@octo.com>
2021-10-19 10:22:19 +0200

    refactor: utilisation du page object pattern

--- __test__/App.test.tsx
+++ __test__/App.test.tsx
@@ -7,22 +7,69 @@ import { App } from "../src/App";
 import userEvent from "@testing-library/user-event";
 
 describe("<App /> tests de caractérisation", function () {
-  it("permet d'ajouter et de cocher des todos", () => {
-    render(<App />);
+  let app: AppPageObject;
 
-    userEvent.type(screen.getByLabelText("Add a new todo"), "Buy bread");
-    userEvent.click(screen.getByRole("button", { name: "Add" }));
-    expect(
-      screen.getByRole("checkbox", { name: "Buy bread" })
-    ).not.toBeChecked();
+  beforeEach(() => (app = new AppPageObject()));
 
-    userEvent.type(screen.getByLabelText("Add a new todo"), "Buy milk");
-    userEvent.click(screen.getByRole("button", { name: "Add" }));
-    expect(
-      screen.getByRole("checkbox", { name: "Buy milk" })
-    ).not.toBeChecked();
+  it("permet d'ajouter des todos", () => {
+    app
+      .givenAnEmptyTodoList()
+      .whenNewTodosAreAdded("Buy bread", "Buy milk")
+      .thenTheyAppearUnchecked("Buy bread", "Buy milk");
+  });
 
-    userEvent.click(screen.getByRole("checkbox", { name: "Buy milk" }));
-    expect(screen.getByRole("checkbox", { name: "Buy milk" })).toBeChecked();
+  it("permet de cocher des todos", () => {
+    app
+      .givenATodoList("Buy bread", "Buy milk")
+      .whenTodoIsToggled("Buy milk")
+      .thenTheyAppearChecked("Buy milk");
   });
 });
+
+class AppPageObject {
+  givenAnEmptyTodoList() {
+    render(<App />);
+
+    return this;
+  }
+
+  givenATodoList(...todos: string[]) {
+    render(<App />);
+
+    this.whenNewTodosAreAdded(...todos);
+
+    return this;
+  }
+
+  whenNewTodosAreAdded(...todos: string[]) {
+    todos.forEach((todo) => {
+      userEvent.type(screen.getByLabelText("Add a new todo"), todo);
+      userEvent.click(screen.getByRole("button", { name: "Add" }));
+    });
+
+    return this;
+  }
+  whenTodoIsToggled(...todos: string[]) {
+    todos.forEach((todo) =>
+      userEvent.click(screen.getByRole("checkbox", { name: todo }))
+    );
+
+    return this;
+  }
+
+  thenTheyAppearUnchecked(...todos: string[]) {
+    todos.forEach((todo) =>
+      expect(screen.getByRole("checkbox", { name: todo })).not.toBeChecked()
+    );
+
+    return this;
+  }
+
+  thenTheyAppearChecked(...todos: string[]) {
+    todos.forEach((todo) =>
+      expect(screen.getByRole("checkbox", { name: todo })).toBeChecked()
+    );
+
+    return this;
+  }
+}
