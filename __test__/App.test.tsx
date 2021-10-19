27be28515dd0bd9b57e26cd44f0a31cb5c8c2e46
Antoine Chalifour <antoine.chalifour@octo.com>
2021-10-19 09:56:40 +0200

    test: améliore le coverage (ajout de nouveau todos)

--- __test__/App.test.tsx
+++ __test__/App.test.tsx
@@ -2,11 +2,15 @@
  * @jest-environment jsdom
  */
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { App } from "../src/App";
+import userEvent from "@testing-library/user-event";
 
 describe("<App /> tests de caractérisation", function () {
   it("renders without crashing", () => {
     render(<App />);
+
+    userEvent.type(screen.getByLabelText("Add a new todo"), "Buy bread");
+    userEvent.click(screen.getByRole("button", { name: "Add" }));
   });
 });
